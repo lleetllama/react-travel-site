5800ef8dd7c439132f4e10694738dfdc453cd8ea
lleetllama <timabryant@yahoo.com>
2022-03-22 22:11:00 -0400

    Initial Commit
    Simple react site built from tutorial.
    https://www.youtube.com/watch?v=I2UBjN5ER4s&ab_channel=BrianDesign

---
+++ src/App.js
@@ -0,0 +1,24 @@
+import React from "react";
+import Navbar from "./components/Navbar";
+import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
+import "./App.css";
+import Home from "./components/pages/Home";
+import Products from "./components/pages/Products";
+import Services from "./components/pages/Services";
+import SignUp from "./components/pages/SignUp";
+
+function App() {
+  return (
+    <Router>
+      <Navbar />
+      <Routes>
+        <Route path="/" element={<Home />} />
+        <Route path="/services" element={<Services />} />
+        <Route path="/products" element={<Products />} />
+        <Route path="/sign-up" element={<SignUp />} />
+      </Routes>
+    </Router>
+  );
+}
+
+export default App;
